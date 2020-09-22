dd8f071a75ed0a98bc1e4d1b16b9a82c8f4f2bcc
Brad Korman <brad.korman@remarque-creative.com>
2020-09-22 02:17:43 -0400

    added screens and fonts

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,24 @@
+//import libraries
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+const CategoryMealScreen = (props) => {
+    return(
+        <View style={styles.screen}>
+            <Text>The Category Meals Screen</Text>
+        </View>
+    )
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+//make this component available to the app
+export default CategoryMealScreen;

17bf43e547d22b1fe504b2d1fcf1a5ed1b684937
Brad Korman <brad.korman@remarque-creative.com>
2020-10-13 09:39:37 -0400

    added filters and switches to filters screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,53 @@
 //import libraries
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Switch } from 'react-native';
+import Colors from '../constants/Colors';
+import Color from '../constants/Colors';
 
 // create a component
+const FilterSwitch = props => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text>{props.label}</Text>
+            <Switch 
+                trackColor={{true: Colors.primary}}
+                value={props.state} 
+                onValueChange={props.onChange} 
+            />
+        </View>
+    )
+}
+
 const FiltersScreen = (props) => {
+    const [isGlutenFree, setIsGlutenFree] = useState(false)
+    const [isLactoseFree, setIsLactoseFree] = useState(false)
+    const [isVegan, setIsVegan] = useState(false)
+    const [isVegetarian, setIsVegetarian] = useState(false)
+
+
     return(
         <View style={styles.screen}>
-            <Text>The Filter Screen</Text>
+            <Text style={styles.title}>Available Filters:</Text>
+            <FilterSwitch 
+                label='Gluten-free' 
+                state={isGlutenFree} 
+                onChange={newValue => setIsGlutenFree(newValue)} 
+            />
+            <FilterSwitch 
+                label='Lactose-free' 
+                state={isLactoseFree} 
+                onChange={newValue => setIsLactoseFree(newValue)} 
+            />
+            <FilterSwitch 
+                label='Vegan' 
+                state={isVegan} 
+                onChange={newValue => setIsVegan(newValue)} 
+            />
+            <FilterSwitch 
+                label='Vegetarian' 
+                state={isVegetarian} 
+                onChange={newValue => setIsVegetarian(newValue)} 
+            />
         </View>
     )
 };
@@ -15,8 +56,20 @@ const FiltersScreen = (props) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+        fontSize: 22,
+        fontFamily: 'open-sans-bold',
+        margin: 20,
+        textAlign: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%',
+        padding: 10
     }
 });
 

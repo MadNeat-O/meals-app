1989b2aa300a5d1865beb34acc81fa49e2ee9c73
Brad Korman <brad.korman@remarque-creative.com>
2020-10-15 10:53:35 -0400

    added fallback text for filtered meal categories

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,5 +1,6 @@
 //import libraries
 import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import MealList from '../components/MealList';
@@ -7,15 +8,32 @@ import MealList from '../components/MealList';
 // create a component
 const CategoryMealScreen = (props) => {
     const catId = props.navigation.getParam('categoryId');
-
     const availableMeals = useSelector(state => state.meals.filteredMeals )
-
     const displayedMeals = availableMeals.filter(meal =>  meal.categoryIds.indexOf(catId) >= 0);
 
+    if (displayedMeals.length === 0) {
+        return (
+            <View style={styles.screen}>
+                <Text>
+                    No available meals based on your filter settings.
+                </Text>
+            </View>
+        )
+    }
     return(
         <MealList displayedMeals={displayedMeals} navigation={props.navigation} />
     )
 };
+
+//Stylesheet
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontFamily: 'open-sans'
+    }
+});
 
 //make this component available to the app
 export default CategoryMealScreen;
